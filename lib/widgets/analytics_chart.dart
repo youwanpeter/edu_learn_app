9584d4f3893566f4136d23a510fc08fda3c3a123
youwanpeter <youwanpeter226@gmail.com>
2025-12-15 21:36:22 +0530

    dashboard change

--- lib/widgets/analytics_chart.dart
+++ lib/widgets/analytics_chart.dart
@@ -1,35 +1,70 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class AnalyticsChart extends StatelessWidget {
+class AnalyticsChart extends StatefulWidget {
   const AnalyticsChart({super.key});
 
   @override
+  State<AnalyticsChart> createState() => _AnalyticsChartState();
+}
+
+class _AnalyticsChartState extends State<AnalyticsChart>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    )..forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return LineChart(
-      LineChartData(
-        gridData: FlGridData(show: false),
-        titlesData: FlTitlesData(show: false),
-        borderData: FlBorderData(show: false),
-        lineBarsData: [
-          LineChartBarData(
-            spots: const [
-              FlSpot(0, 20),
-              FlSpot(1, 35),
-              FlSpot(2, 30),
-              FlSpot(3, 55),
-              FlSpot(4, 48),
-              FlSpot(5, 60),
-              FlSpot(6, 70),
-            ],
-            isCurved: true,
-            barWidth: 4,
-            dotData: FlDotData(show: false),
-            gradient: const LinearGradient(
-              colors: [Colors.white, Colors.white70],
+    return FadeTransition(
+      opacity: _controller,
+      child: LineChart(
+        LineChartData(
+          gridData: FlGridData(show: false),
+          titlesData: FlTitlesData(show: false),
+          borderData: FlBorderData(show: false),
+          lineBarsData: [
+            LineChartBarData(
+              spots: const [
+                FlSpot(0, 20),
+                FlSpot(1, 35),
+                FlSpot(2, 30),
+                FlSpot(3, 55),
+                FlSpot(4, 48),
+                FlSpot(5, 60),
+                FlSpot(6, 70),
+              ],
+              isCurved: true,
+              barWidth: 4,
+              dotData: FlDotData(show: false),
+              belowBarData: BarAreaData(
+                show: true,
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.white.withOpacity(0.4),
+                    Colors.white.withOpacity(0.05),
+                  ],
+                ),
+              ),
+              gradient: const LinearGradient(
+                colors: [Colors.white, Colors.white70],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/widgets/progress_card.dart
+++ lib/widgets/progress_card.dart
@@ -1,28 +1,51 @@
 import 'package:flutter/material.dart';
 import '../models/course_progress.dart';
 
-class ProgressCard extends StatelessWidget {
+class ProgressCard extends StatefulWidget {
   final CourseProgress course;
 
   const ProgressCard({super.key, required this.course});
+
+  @override
+  State<ProgressCard> createState() => _ProgressCardState();
+}
+
+class _ProgressCardState extends State<ProgressCard>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _progressAnim;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 900),
+    );
+
+    _progressAnim = Tween<double>(
+      begin: 0,
+      end: widget.course.progress,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic));
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
-        gradient: LinearGradient(
-          colors: [
-            Colors.white.withOpacity(0.9),
-            Colors.white.withOpacity(0.6),
-          ],
-        ),
+        color: Colors.white,
         boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.05),
-            blurRadius: 10,
-            offset: const Offset(0, 5),
-          ),
+          BoxShadow(color: Colors.black.withOpacity(0.05), blurRadius: 12),
         ],
       ),
       padding: const EdgeInsets.all(18),
@@ -30,22 +53,30 @@ class ProgressCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            course.courseName,
+            widget.course.courseName,
             style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
           ),
           const SizedBox(height: 14),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: LinearProgressIndicator(
-              value: course.progress,
-              minHeight: 8,
-              backgroundColor: Colors.grey.shade200,
-              color: Colors.blueAccent,
-            ),
+
+          /// 🔥 Animated Progress
+          AnimatedBuilder(
+            animation: _progressAnim,
+            builder: (_, __) {
+              return ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: LinearProgressIndicator(
+                  value: _progressAnim.value,
+                  minHeight: 8,
+                  backgroundColor: Colors.grey.shade200,
+                  color: Colors.blueAccent,
+                ),
+              );
+            },
           ),
+
           const SizedBox(height: 8),
           Text(
-            "${course.completedLessons}/${course.totalLessons} lessons completed",
+            "${widget.course.completedLessons}/${widget.course.totalLessons} lessons completed",
             style: const TextStyle(color: Colors.grey, fontSize: 13),
           ),
         ],
